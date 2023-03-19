46482419deebb7cc9e5c82e6f5ecf4af78d2e498
Shahar Saadon <shaharsaadon1@gmail.com>
2023-03-19 13:55:00 +0200

    render atlas

--- config/prod.js
+++ config/prod.js
@@ -1,4 +1,11 @@
+require('dotenv').config()
+
 module.exports = {
-  dbURL: 'mongodb+srv://theUser:thePass@cluster0-klgzh.mongodb.net/test?retryWrites=true&w=majority',
-  dbName: 'toy_db'
+  // dbURL: process.env.ATLAS_URL,
+  // dbName: process.env.ATLAS_DB_NAME,
+  ATLAS_URL: "mongodb+srv://mister_toy:shahar1234@cluster0.qxhbqur.mongodb.net/?retryWrites=true&w=majority",
+  ATLAS_DB_NAME: "toy_db"
+
+
 }
+
