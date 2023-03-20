d5b2f3ca28ac3dfec6d551c337d1cc5a403a3fee
Shahar Saadon <shaharsaadon1@gmail.com>
2023-03-20 16:13:11 +0200

    fix db

--- config/prod.js
+++ config/prod.js
@@ -1,11 +1,10 @@
 require('dotenv').config()
 
 module.exports = {
-  // dbURL: process.env.ATLAS_URL,
-  // dbName: process.env.ATLAS_DB_NAME,
-  ATLAS_URL: "mongodb+srv://mister_toy:shahar1234@cluster0.qxhbqur.mongodb.net/?retryWrites=true&w=majority",
-  ATLAS_DB_NAME: "toy_db"
-
+  dbURL: process.env.ATLAS_URL,
+  dbName: process.env.ATLAS_DB_NAME,
+  // dbURL: "mongodb+srv://mister_toy:shahar1234@cluster0.qxhbqur.mongodb.net/?retryWrites=true&w=majority",
+  // dbName: "toy_db"
 
 }
 
